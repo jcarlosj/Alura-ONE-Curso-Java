e91b594932c03f3764e9700e0e4210d6d238e7b3
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-12-01 11:50:50 -0500

    [+] Class (Account): Metodos (depositar, retirar, transfeir) & Composicion: 'Account' <- 'Client'

--- src/bitebank/Account.java
+++ src/bitebank/Account.java
@@ -3,10 +3,44 @@ package bitebank;
 public class Account {
     private String id;
     private String name_bank;
+    private Client holder;
+    private double balance;
 
-    Account( String name_bank ) {
+    Account( String name_bank, String owner_id, String owner_name, String owner_phone ) {
         this.id = String.valueOf( System.currentTimeMillis() );
         this.name_bank = name_bank;
+        this.holder = new Client( owner_id, owner_name, owner_phone );
+    }
+
+    public boolean withdraw_amount( double value ) {
+        if( this.balance >= value ) {
+            this.balance -= value;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean deposit_amount( double value ) {
+        if( value > 0 ) {
+            this.balance += value;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean transfer_amount( double value, Account destination_account ) {
+        if( this.balance >= value ) {
+            this.withdraw_amount( value );
+            destination_account.deposit_amount( value );
+
+            return true;
+        }
+
+        return false;
     }
 
     @Override
@@ -14,6 +48,8 @@ public class Account {
         return "Account{" +
                 "id='" + id + '\'' +
                 ", name_bank='" + name_bank + '\'' +
+                ", holder=" + holder +
+                ", balance=" + balance +
                 '}';
     }
 }
--- src/bitebank/ByteBank.java
+++ src/bitebank/ByteBank.java
@@ -3,16 +3,9 @@ package bitebank;
 public class ByteBank {
     public static void main(String[] args) {
         // Create Accounts
-        Account bba_1 = new Account( "ByteBank" );
-        System.out.println( bba_1 );
-        Account bba_2 = new Account( "ByteBank" );
+        Account bba_1 = new Account( "ByteBank", "187632321","Sofia", "2331021" );
+        Account bba_2 = new Account( "ByteBank", "154579925","Eva", "2115694"  );
         System.out.println( bba_1 );
         System.out.println( bba_2 );
-
-        // Create Clients
-        Client client_1 = new Client("187632321","Sofia", "2331021" );
-        Client client_2 = new Client( "154579925","Eva", "2115694" );
-        System.out.println( client_1 );
-        System.out.println( client_2 );
     }
 }
