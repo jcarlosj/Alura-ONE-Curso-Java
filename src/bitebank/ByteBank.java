d0b5140532869980a214b3b9aca99d5349eadb03
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-12-05 17:12:16 -0500

    [+] Main: Account ArrayList

--- src/bitebank/ByteBank.java
+++ src/bitebank/ByteBank.java
@@ -1,18 +1,20 @@
 package bitebank;
 
+import java.util.ArrayList;
+
 public class ByteBank {
     public static void main(String[] args) {
-        Account[] accounts = new Account[ 5 ];      // Array de tipo cuenta
+        ArrayList<Account> accountsList = new ArrayList<Account>(); // ArrayList de tipo cuenta
 
         // Create Accounts
         Account sofia_account = new CurrentAccount( "ByteBank", "187632321","Sofia", "2331021", 5000 );
         Account eva_account = new SavingsAccount( "ByteBank", "154579925","Eva", "2115694"  );
 
-        accounts[ 1 ] = sofia_account;
-        accounts[ 2 ] = eva_account;
+        accountsList.add( sofia_account );
+        accountsList.add( eva_account );
 
-        for( int i = 0; i < accounts.length; i++ ) {
-            System.out.println( accounts[ i ] );
+        for( Account account : accountsList ) {
+            System.out.println( account );
         }
     }
 }
