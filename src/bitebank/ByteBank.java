410913a5171b459435cb74ece81b4cd83809c3b8
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-12-05 14:05:48 -0500

    [+] Main: Account Array

--- src/bitebank/ByteBank.java
+++ src/bitebank/ByteBank.java
@@ -2,26 +2,17 @@ package bitebank;
 
 public class ByteBank {
     public static void main(String[] args) {
+        Account[] accounts = new Account[ 5 ];      // Array de tipo cuenta
+
         // Create Accounts
         Account sofia_account = new CurrentAccount( "ByteBank", "187632321","Sofia", "2331021", 5000 );
         Account eva_account = new SavingsAccount( "ByteBank", "154579925","Eva", "2115694"  );
-        System.out.println( "Valores iniciales" );
-        System.out.println( sofia_account );
-        System.out.println( eva_account );
 
-        // Solo un try/catch es quien realmente se hace cargo de manejar la Exception
-        try {
-            sofia_account.transfer_amount( 2000, eva_account );
-            eva_account.withdraw_amount( 500 );
-            eva_account.withdraw_amount( 2500 );        // -> Producirá InsufficientBalanceException, Excepción personalizada (Checked Exception)
-            eva_account.withdraw_amount( 50 );
-            eva_account.withdraw_amount( 50 );
-        } catch ( InsufficientBalanceException e ) {
-            e.printStackTrace();
+        accounts[ 1 ] = sofia_account;
+        accounts[ 2 ] = eva_account;
+
+        for( int i = 0; i < accounts.length; i++ ) {
+            System.out.println( accounts[ i ] );
         }
-
-        System.out.println( "Valores finales" );
-        System.out.println( sofia_account );
-        System.out.println( eva_account );
     }
 }
