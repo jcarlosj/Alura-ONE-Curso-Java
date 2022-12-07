f26ce11031c0147b5d096c3a236eca945005d14d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-12-07 10:02:00 -0500

    [$] Class (SortClientById): Forma basica a forma corta

--- src/bitebank/SortClientById.java
+++ src/bitebank/SortClientById.java
@@ -5,7 +5,8 @@ import java.util.Comparator;
 public class SortClientById implements Comparator<Account> {
     @Override
     public int compare( Account account, Account t1 ) {
-        if( Integer.parseInt( account.getHolder().getId() ) == Integer.parseInt( t1.getHolder().getId() ) ) {
+        // FORMA 1 (Básica): Retornará 0 (Cero): Si es igual, 1 (Uno): Si es mayor, -1 (Menos uno): Si es menor
+        /*if( Integer.parseInt( account.getHolder().getId() ) == Integer.parseInt( t1.getHolder().getId() ) ) {
             return 0;
         }
         else if( Integer.parseInt( account.getHolder().getId() ) > Integer.parseInt( t1.getHolder().getId() ) ) {
@@ -13,6 +14,9 @@ public class SortClientById implements Comparator<Account> {
         }
         else {
             return -1;
-        }
+        }*/
+
+        // FORMA 2 (Corta): Retornará 0 (Cero): Si es igual, # (Un positivo): Si es mayor, -# (Un negativo): Si es menor
+        return Integer.parseInt( account.getHolder().getId() ) - Integer.parseInt( t1.getHolder().getId() );
     }
 }
