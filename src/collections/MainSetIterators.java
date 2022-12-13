599a4cdd2da512e0358cb29baa54147228d6d5e8
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-12-13 09:57:24 -0500

    [+] MainSetIterator: Recorrer un Objeto usando un Iterador

---
+++ src/collections/MainSetIterators.java
@@ -0,0 +1,42 @@
+package collections;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class MainSetIterators {
+    // TODO: https://docs.oracle.com/javase/tutorial/collections/interfaces/index.html
+
+    public static void main(String[] args) {
+        Collection<Student> students = new HashSet<Student>(){
+            {
+                add( new Student( "Juan Carlos", "87634" ) );
+                add( new Student( "Maria", "69322" ) );
+                add( new Student( "Maria", "72314" ) );
+                add( new Student( "Carolina", "23093" ) );
+                add( new Student( "Juan", "45391" ) );
+            }
+        };
+
+        students.add( new Student( "Ana Maria", "54931" ) );
+        students.add( new Student( "Juan Carlos", "87634" ) );
+
+        Course python_course = new Course( "Python Basico", 50 );
+
+        for( Object student : students ) {
+            python_course.addStudent( ( Student ) student );
+        }
+
+        System.out.println( "Listado alumnos curso de Python (for)" );
+        python_course.getStudentsList().forEach( student -> System.out.println( "\t> " + student ) );
+
+        // Iterator: es una forma de como vamos a leer una lista
+        Iterator<Student> studentsIterator = python_course.getStudentsList().iterator();
+
+        System.out.println( "Listado alumnos curso de Python (Iterator)" );
+        while ( studentsIterator.hasNext() ) {
+            System.out.println( "\t> " + studentsIterator.next() );
+        }
+
+    }
+}
